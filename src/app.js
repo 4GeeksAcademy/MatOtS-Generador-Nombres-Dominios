477dd62975e79c6a6793f44c0f5fa9356ba0bf08
Matias Otero Sarra <76122487+MatOtS@users.noreply.github.com>
2026-01-26 20:54:11 +0000

    extra del ejercicio

--- src/app.js
+++ src/app.js
@@ -17,8 +17,15 @@ window.onload = function() {
   for (let i=0;i<pronoun.length;i++) {
     for (let j=0;j<adj.length;j++) {
       for (let k=0;k < noun.length;k++) {
-        for (let h=0;h<domain.length;h++)
-          console.log(`${pronoun[i]+adj[j]+noun[k]+domain[h]}`)
+        for (let h=0;h<domain.length;h++) {
+          if (`${noun[k]}`.includes(`${domain[h]}`.split(".")[1])) {
+            let newDom = noun[k].split(domain[h].split(".")[1])
+            console.log(`${pronoun[i]+adj[j]+noun[k].split(domain[h].split(".")[1])[0]+domain[h]}`)
+          }
+          else {
+            console.log(`${pronoun[i]+adj[j]+noun[k]+domain[h]}`)
+          }
+        }
       }
     }
   }
