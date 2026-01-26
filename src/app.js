493fd6526a882beab4377ad40d8883b6870a8b5b
Matias Otero Sarra <76122487+MatOtS@users.noreply.github.com>
2026-01-26 20:30:04 +0000

    Domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon','capcom','fungus','bonus'];
+  let domain = ['.com','.net','.us','.io']
+
+
+
+  for (let i=0;i<pronoun.length;i++) {
+    for (let j=0;j<adj.length;j++) {
+      for (let k=0;k < noun.length;k++) {
+        for (let h=0;h<domain.length;h++)
+          console.log(`${pronoun[i]+adj[j]+noun[k]+domain[h]}`)
+      }
+    }
+  }
+
+
 };
